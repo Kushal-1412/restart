8d6d0ede4073cfafdf1519af88287c10bbcea062
Kushal-1412 <Kushal@gmail.com>
2023-04-17 11:32:54 +0000

    second

--- restart.ps1
+++ restart.ps1
@@ -1,3 +1,8 @@
-#hiii
+#stop server
+cmd.exe /c echo y | ssh -i LinuxKey7.pem ec2-user@43.205.117.181 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/shutdown.sh
 
+#catch clear
+cmd.exe /c echo y | ssh -i LinuxKey7.pem ec2-user@43.205.117.181 sudo rm -rf /mnt/webserver/apache-tomcat-9.0.73/temp
 
+#start server
+cmd.exe /c echo y | ssh -i LinuxKey7.pem ec2-user@43.205.117.181 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/startup.sh
