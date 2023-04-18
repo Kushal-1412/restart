ed355f0cb7472b44b122eaa1d4ad31adf0ddab55
Kushal-1412 <Kushal@gmail.com>
2023-04-18 15:36:57 +0000

    final

--- restart.ps1
+++ restart.ps1
@@ -1,8 +1,9 @@
 #stop server
-cmd.exe /c echo y | ssh -i C:\Users\SHREE\LinuxKey7.pem ec2-user@43.205.117.181 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/shutdown.sh
+cmd.exe /c echo y | ssh -i "C:\Keys\LinuxKey7.pem" ec2-user@43.204.28.201 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/shutdown.sh
 
 #catch clear
-cmd.exe /c echo y | ssh -i C:\Users\SHREE\LinuxKey7.pem ec2-user@43.205.117.181 sudo rm -rf /mnt/webserver/apache-tomcat-9.0.73/temp
+cmd.exe /c echo y | ssh -i "C:\Keys\LinuxKey7.pem" ec2-user@43.204.28.201 sudo rm -rf /mnt/webserver/apache-tomcat-9.0.73/temp
 
 #start server
-cmd.exe /c echo y | ssh -i C:\Users\SHREE\LinuxKey7.pem ec2-user@43.205.117.181 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/startup.sh
+cmd.exe /c echo y | ssh -i "C:\Keys\LinuxKey7.pem" ec2-user@43.204.28.201 sudo /mnt/webserver/apache-tomcat-9.0.73/bin/startup.sh
+
